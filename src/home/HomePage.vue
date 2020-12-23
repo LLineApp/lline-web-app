4e1f3dac4c8991f1d32355d9cd5af62abb0809bc
RenatoDaSilva <renato.katia@gmail.com>
2020-12-23 00:40:04 -0300

    adds quiz components

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-        <NavBar />
-<h1>Bem vindo!</h1>
+    <NavBar />
+    <h1>Bem vindo!</h1>
     <p>Agora você faz parte do LLine</p>
+    <Quiz />
   </div>
 </template>
 
 <script>
 import NavBar from "../navbar/NavBar";
+import Quiz from "../quiz/Quiz";
 
 export default {
   computed: {},
-  components: { NavBar },
+  components: { NavBar, Quiz },
   created() {},
   methods: {},
 };
---
+++ src/quiz/Intro.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <p>
+      Olá. Parece que você acabou de se inscrever, e eu não tenho muitas
+      informações a seu respeito. Para que possamos dar continuidade ao
+      processo, será necessário eu fazer algumas perguntas. As informações
+      obtidas aqui são de sigilo absoluto, e você pode interromper o
+      preenchimento a hora que quiser, e continuar mais tarde se assim precisar
+      <b>Mais tarde precisaremos melhorar esse texto</b>
+      <br />
+      <button class="btn btn-primary" v-on:click="$emit('didAccept')">
+        OK, vamos começar
+      </button>
+      <button class="btn btn-secondary" v-on:click="$emit('didNotAccept')">
+        Não quero
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+export default {
+  data() {
+    return {
+      quizData: {
+        accepted: false,
+      },
+    };
+  },
+  methods: {
+    handleAccept(e) {
+      this.quizData.accepted = true;
+      this.setData();
+    },
+    handleDontAccept(e) {
+      this.quizData.accepted = false;
+      this.setData();
+    },
+    setData() {
+      sessionStorage.setItem("quizData", JSON.stringify(this.quizData));
+    },
+  },
+};
+</script> 
