58f3d69faadc8a907d3f887e9550ee140a226623
RenatoDaSilva <renato.katia@gmail.com>
2020-12-28 21:59:11 -0300

    adds phone list

--- src/profile/Email.vue
+++ src/profile/Email.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <label for="fullName">Nome completo</label>
-    <input
-      type="text"
-      v-model="profileData.fullName"
-      v-validate="'required'"
-      name="fullName"
-      class="form-control"
-      :class="{ 'is-invalid': submitted && errors.has('fullName') }"
-    />
-    <div v-if="submitted && errors.has('fullName')" class="invalid-feedback">
-      {{ errors.first("fullName") }}
-    </div>
     <label for="email">E-mail</label>
     <input
       type="text"
@@ -24,6 +12,53 @@
     <div v-if="submitted && errors.has('email')" class="invalid-feedback">
       {{ errors.first("email") }}
     </div>
+    <label for="fullName">Nome completo</label>
+    <input
+      type="text"
+      v-model="profileData.fullName"
+      v-validate="'required'"
+      name="fullName"
+      class="form-control"
+      :class="{ 'is-invalid': submitted && errors.has('fullName') }"
+    />
+    <div v-if="submitted && errors.has('fullName')" class="invalid-feedback">
+      {{ errors.first("fullName") }}
+    </div>
+
+    <label for="birthDate">Data de nascimento</label>
+    <input
+      type="date"
+      v-model="profileData.birthDate"
+      v-validate="'required'"
+      name="birthDate"
+      class="form-control"
+      formatter="formatDate"
+      :class="{ 'is-invalid': submitted && errors.has('birthDate') }"
+    />
+    <div v-if="submitted && errors.has('birthDate')" class="invalid-feedback">
+      {{ errors.first("birthDate") }}
+    </div>
+    <label for="phoneInput"
+      >Informe aqui todos os seus números de telefone com DDD, um por um</label
+    >
+    <input type="text" v-model="phoneInput" v-on:keyup.enter="addPhone()" name="phoneInput" class="form-control" />
+    <button class="btn btn-info" v-on:click="addPhone()">Adicionar</button>
+
+    <b-form-group
+      label="Informe qual é o seu telefone preferencial"
+      v-slot="{ ariaDescribedby }"
+    >
+      <b-form-radio-group
+        id="phone-radio-slots"
+        v-model="profileData.preferredPhone"
+        :options="profileData.phones"
+        :aria-describedby="ariaDescribedby"
+        name="radio-options-slots"
+      >
+      </b-form-radio-group>
+    </b-form-group>
+
+    <br />
     <input
       type="checkbox"
       v-model="profileData.requestBrokerStatus"
@@ -46,6 +81,7 @@
 <script>
 import { mapState, mapActions } from "vuex";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { parsePhoneNumber } from "libphonenumber-js";
 
 export default {
   data() {
@@ -53,13 +89,30 @@ export default {
       profileData: {
         fullName: "",
         email: "",
+        birthDate: null,
+        phones: [],
+        preferredPhone: "",
         requestBrokerStatus: false,
       },
       submitted: false,
+      phoneInput: "",
     };
   },
   computed: {
     ...mapState("account", ["status"]),
+    formatDate(value, event) {
+      return moment(value).format("DD/MM/YYYY");
+    },
+  },
+  methods: {
+    addPhone(value, event) {
+      this.phoneInput = this.phoneInput.replace(/\D+/g, "");
+      if (this.phoneInput) {
+        const phoneParsed = parsePhoneNumber(this.phoneInput, "BR");
+        this.profileData.phones.push(phoneParsed.formatNational());
+        this.phoneInput = '';
+      }
+    },
   },
 };
 </script> 
