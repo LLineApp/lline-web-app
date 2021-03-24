ffea2fef69c620c35b73018a6ca6deb7d63814cf
Renato da Silva <renato.katia@gmail.com>
2021-03-23 21:05:05 -0300

    Adds advisor's portfolio page #94

---
+++ src/Advisor/Portfolio.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-table hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "portfolio",
+  data() {
+    return {
+
+         fields: [
+          {
+            key: 'fullname',
+            label: "Nome",
+            sortable: true
+          },
+          {
+            key: 'email',
+            label: "E-mail",
+            sortable: true
+          },
+          {
+            key: 'preferredContact',
+            label: 'Telefone',
+            sortable: true
+          }
+        ],
+      items: [{
+        fullname: "",
+        email: "",
+        preferredContact: "",
+      }],
+    };
+  },
+};
+</script>
+
+<style></style>
