e3184d8c93dabb481d576293eade4717f5e01f3b
Renato da Silva <renato.katia@gmail.com>
2021-06-01 21:04:42 -0300

    Shows CPF field on clients profile sheet #189

--- src/profile/ProfileDataSheet.vue
+++ src/profile/ProfileDataSheet.vue
@@ -51,6 +51,7 @@
         <Email
           v-on:setActiveComponent="setActiveComponent"
           v-bind:recordedData="profileData"
+          v-bind:isClientData="isClientProfile"
           v-on:done="feedProfileData"
           v-if="this.page == 2"
         />
@@ -158,7 +159,6 @@ export default {
     return {
       key: 0,
       page: 2,
-      isClientProfile: false,
       title: "Estes são os seus dados",
       activeComponentName: "",
       showAlert: false,
@@ -174,12 +174,7 @@ export default {
     };
   },
   created() {
-    const cpf = this.$route.params.clientCpf;
-    if (cpf) {
-      this.isClientProfile = true;
-    }
-
-    getProfile(this.$cookies.get("token"), cpf)
+    getProfile(this.$cookies.get("token"), this.$route.params.clientCpf)
       .then((data) => {
         this.profileData = data.data.getProfile[0];
         if (this.isClientProfile) {
@@ -198,6 +193,11 @@ export default {
 
         this.$toasted.error(message, options);
       });
+  },
+  computed: {
+    isClientProfile: function() {
+      return Boolean(this.$route.params.clientCpf);
+    },
   },
   methods: {
     goBack() {
