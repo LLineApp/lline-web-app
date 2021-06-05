839acfe629acae9461def9c53a9ab6e4d08c7681
Renato da Silva <renato.katia@gmail.com>
2021-06-05 00:53:57 -0300

    Removes users.module and account.module and its relations fro project #193

--- src/_helpers/auth-header.js
+++
@@ -1,7 +0,0 @@
-import { loginData } from "../_store/login.module";
-
-export function authHeader() {
-  return loginData.state.data.token
-    ? { Authorization: "Bearer " + loginData.state.data.token }
-    : {};
-}
