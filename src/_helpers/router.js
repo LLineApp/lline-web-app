d35742a55b54806b6047c7b3d2978e8a1ba8aa62
Renato da Silva <renato.katia@gmail.com>
2021-06-10 22:04:01 -0300

    Fixes logout when refreshes page #198

--- src/_helpers/router.js
+++ src/_helpers/router.js
@@ -8,7 +8,7 @@ import ProfileDataSheet from "../profile/ProfileDataSheet";
 import Client from "../profile/Client";
 import Portfolio from "../advisor/Portfolio";
 import Prospect from "../advisor/Prospect";
-import { loginData } from "../_store/login.module";
+import { store } from "../_store/index";
 
 Vue.use(Router);
 
@@ -30,7 +30,7 @@ export const router = new Router({
 router.beforeEach((to, from, next) => {
   const publicPages = ["/login", "/register"];
   const authRequired = !publicPages.includes(to.path);
-  if (authRequired && !loginData.state.login.token) {
+  if (authRequired && !store.state.loginData.login.token) {
     return next("/login");
   }
   next();
