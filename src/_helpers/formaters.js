148e56f2e87b049d3a8dd707e987390f25e69da4
Renato da Silva <renato.katia@gmail.com>
2021-11-25 20:40:29 -0300

    fixes formaters and their tests #260 (#262)

--- src/_helpers/formaters.js
+++ src/_helpers/formaters.js
@@ -1,11 +1,29 @@
 export function formatNumericField(value) {
-  return parseFloat(value);
+  if (value) {
+    if (typeof value == "string") {
+      value = value.replace(/[^0-9\.]/g, "");
+      value = value === "" ? (0.0).toFixed(2) : value;
+      value = isNaN(value) ? (0.0).toFixed(2) : value;
+      return parseFloat(value).toFixed(2);
+    }
+    if (typeof value == "number") {
+      return parseFloat(value).toFixed(2);
+    }
+  }
+  return (0.0).toFixed(2);
 }
 
 export function formatIntField(value) {
   if (value) {
-    value = value.replace(/\D/g, "");
-    value = isNaN(value) ? 0 : value;
-    return parseInt(value);
+    if (typeof value == "string") {
+      value = value.replace(/[^0-9\.]/g, "");
+      value = value === "" ? 0 : value;
+      value = isNaN(value) ? 0 : value;
+      return parseInt(value);
+    }
+    if (typeof value == "number") {
+      return parseInt(value);
+    }
   }
+  return 0;
 }
